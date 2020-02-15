6c65e7185bff1d01b93170c8a634d7da0141645c
Bryce Tucker <bryce.tucker@gmail.com>
2020-02-15 01:12:27 +0000

    Established one route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :articles
+    end
+  end
+  
   devise_for :users,
               path: '',
               path_names: {
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_232751) do
+ActiveRecord::Schema.define(version: 2020_02_15_002028) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "slug"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "jwt_blacklists", force: :cascade do |t|
     t.string "jti"
